860bc0f25f0cae3e1ff6096c6185be4030eb19d3
basamma406 <101272950+basamma406@users.noreply.github.com>
2022-04-23 12:43:11 +0530

    Add files via upload

---
+++ springdatajpa-1/src/main/java/com/ust/springboot/data/repos/ProductRepository.java
@@ -0,0 +1,18 @@
+package com.ust.springboot.data.repos;
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ust.springboot.data.entities.Product;
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
+
+	List<Product> findByName(String name);
+
+	List<Product> findByPrice(Double price);
+
+	List<Product> findByNameAndPrice(String name, Double price);
+}
+
+
+
